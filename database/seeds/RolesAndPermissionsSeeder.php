08545c6d621a59db0f05d9baaeb84ff27dc8ec71
iftekharul <iftekharul@augnitive.com>
2020-08-13 12:36:08 +0600

    Hot fix

--- database/seeds/RolesAndPermissionsSeeder.php
+++ database/seeds/RolesAndPermissionsSeeder.php
@@ -26,8 +26,6 @@ class RolesAndPermissionsSeeder extends Seeder
 
         $address = Address::create([
             'address' => null,
-            'address_line_1' => null,
-            'address_line_2' => null,
             'city' => null,
             'post_code' => null
         ]);
