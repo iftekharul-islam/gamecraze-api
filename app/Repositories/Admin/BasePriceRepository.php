1208896a262a42a15143ac34d337d9aae75d0f88
iftekharul <iftekharul@augnitive.com>
2021-04-25 00:49:29 +0600

    pricing check by log

--- app/Repositories/Admin/BasePriceRepository.php
+++ app/Repositories/Admin/BasePriceRepository.php
@@ -144,6 +144,7 @@ class basePriceRepository
                 'discount_commission' => config('gamehub.offer_on_digital_game') == true ?
                     $digital_commission - $digitalDiscountCommission : $digital_commission,
             ];
+            logger($price);
             if ($achieveDiscount == true){
                 $price['discount_price'] = $price['regular_price'];
                 $price['discount_commission'] = $price['regular_commission'];
