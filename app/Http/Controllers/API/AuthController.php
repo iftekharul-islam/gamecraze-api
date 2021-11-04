a98fe92e03fed6dfa4e67266f6600b0db2da3e00
iftekharul-dev <iftekharul@augnitive.com>
2021-11-04 20:25:15 +0600

    Header add

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -87,10 +87,6 @@ class AuthController extends BaseController
      */
     public function login(UserLoginRequest $request)
     {
-//        header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS, post, get');
-//        header("Access-Control-Max-Age", "3600");
-//        header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
-//        header("Access-Control-Allow-Credentials", "true");
         $token = $this->loginService->login($request);
         if ($token == false) {
             return $this->response->array([
--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -8,4 +8,14 @@ use Illuminate\Http\Request;
 class BaseController extends Controller
 {
     Use Helpers;
+
+    public function __construct()
+    {
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS, post, get');
+        header("Access-Control-Max-Age", "3600");
+        header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+        header("Access-Control-Allow-Credentials", "true");
+
+    }
 }
